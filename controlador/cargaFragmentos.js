c6c547549dc9658ab1228e110a252d16a3bf4306
Vina del Mar <vina@duoc.acad>
2024-04-13 10:00:40 -0400

    Implementacion Popover

--- controlador/cargaFragmentos.js
+++ controlador/cargaFragmentos.js
@@ -14,4 +14,9 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
   });
   
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+const popover = new bootstrap.Popover('.popover-dismiss', {
+    trigger: 'focus'
+  })
