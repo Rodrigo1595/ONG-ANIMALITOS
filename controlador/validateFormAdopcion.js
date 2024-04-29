6458721427506033ee1abffffdfc3517a285523c
Rodrigo Amigo Morgado <amigo.rodrigo.morgado@gmail.com>
2024-04-29 15:47:31 -0400

    Cambios Evelyn , formulario adopcion

--- controlador/validateFormAdopcion.js
+++ controlador/validateFormAdopcion.js
@@ -1,3 +1,53 @@
+// $(document).ready(function() {
+//     $("input").focusout(function() {
+
+//         let flag = false
+//         var value = $(this).val();
+//         if (value.length == 0) {
+//             flag = true
+//         } 
+//         if($(this).attr('id') == 'idNombreAdopcion' || $(this).attr('id') == 'idApellidoAdopcion'){
+//             if(value.length < 3 || value.length>20){
+//                 flag=true
+//             }
+//         }
+
+//         if($(this).attr('id') == 'idEmailAdopcion'){
+//             if(value.length < 15 || value.length>100){
+//                 flag=true
+//             }
+//         }
+
+//         if(flag){
+//             $(this).addClass("is-invalid");
+//             if(!$('#idButtonEnviar').attr('disabled')){
+//                 $('#idButtonEnviar').attr('disabled',true)
+//             }
+//         }else{
+//             // Si el campo tiene contenido, elimina la clase 'is-invalid'
+//             $(this).removeClass("is-invalid");
+//             $(this).addClass("is-valid");
+//             $('#idButtonEnviar').removeAttr('disabled')
+//         }
+//     });
+
+//     $("select").focusout(function() {
+//         var value = $(this).val();
+//         if (value.length == 0) {
+//             // Si el campo está vacío, añade la clase 'is-invalid'
+//             $(this).addClass("is-invalid");
+//             //Deshabilitar boton envio
+//             if(!$('#idButtonEnviar').attr('disabled')){
+//                 $('#idButtonEnviar').attr('disabled',true)
+//             }
+//         } else {
+//             $(this).removeClass("is-invalid");
+//             $(this).addClass("is-valid");
+//             $('#idButtonEnviar').removeAttr('disabled')
+//         }
+//     });
+// });
+
 $(document).ready(function() {
     $("input").focusout(function() {
 
@@ -19,15 +69,16 @@ $(document).ready(function() {
         }
 
         if(flag){
+            $(this).removeClass("is-valid");
             $(this).addClass("is-invalid");
-            if(!$('#idButtonEnviar').attr('disabled')){
-                $('#idButtonEnviar').attr('disabled',true)
+            if(!$('.btn-aceptar').attr('disabled')){
+                $('.btn-aceptar').attr('disabled',true)
             }
         }else{
             // Si el campo tiene contenido, elimina la clase 'is-invalid'
             $(this).removeClass("is-invalid");
             $(this).addClass("is-valid");
-            $('#idButtonEnviar').removeAttr('disabled')
+            $('.btn-aceptar').removeAttr('disabled')
         }
     });
 
@@ -35,15 +86,16 @@ $(document).ready(function() {
         var value = $(this).val();
         if (value.length == 0) {
             // Si el campo está vacío, añade la clase 'is-invalid'
+            $(this).removeClass("is-valid");
             $(this).addClass("is-invalid");
             //Deshabilitar boton envio
-            if(!$('#idButtonEnviar').attr('disabled')){
-                $('#idButtonEnviar').attr('disabled',true)
+            if(!$('.btn-aceptar').attr('disabled')){
+                $('.btn-aceptar').attr('disabled',true)
             }
         } else {
             $(this).removeClass("is-invalid");
             $(this).addClass("is-valid");
-            $('#idButtonEnviar').removeAttr('disabled')
+            $('.btn-aceptar').removeAttr('disabled')
         }
     });
 });
